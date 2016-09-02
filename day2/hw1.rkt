23b950f00b90aa7e40f6ea5cfdc0bcd626c39d3e
David Zhu <hdavidzhu@gmail.com>
2016-09-01 22:30:13 -0400

    ADD: Sum definition.

--- day2/hw1.rkt
+++ day2/hw1.rkt
@@ -56,14 +56,17 @@
 (display (fib 6)) (newline) ;; -> 8
 (display (fib 20)) (newline) ;; -> 6765
 
-;;; 5.  WRITE a procedure that takes a list of numbers and returns the sum of those numbers
-;;;     Hint:  first, rest, cons
-;(define (sum lst)
-;  your-code-here)
-;
-;(display (sum '(1 2 3 4))) (newline) ;; -> 10
-;(display (sum '(1 20 300))) (newline) ;; -> 321
-;
+;; 5.  WRITE a procedure that takes a list of numbers and returns the sum of those numbers
+;;     Hint:  first, rest, cons
+(define (sum lst)
+  (if (null? lst)
+      0
+      (+ (list-ref lst 0) (sum (list-tail lst 1)))))
+
+(display "sum testing: ") (newline)
+(display (sum '(1 2 3 4))) (newline) ;; -> 10
+(display (sum '(1 20 300))) (newline) ;; -> 321
+
 ;;; 6.  WRITE a procedure that takes a list of numbers and returns the largest one.
 ;;;     While there are solutions using scheme's built-in max, we were actually hoping you'd do something else...
 ;(define (my-max lst)
