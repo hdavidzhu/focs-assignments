f055b75ded4eaba9c595a0d8d093d4e90f9c5587
David Zhu <hdavidzhu@gmail.com>
2016-09-09 00:13:10 -0400

    ADD: Checkmark for completion.

--- day3/hw2.rkt
+++ day3/hw2.rkt
@@ -3,7 +3,7 @@
 ;;; Student Name: Frankly Olin [change to your name]
 ;;;
 ;;; Check one:
-;;; [ ] I completed this assignment without assistance or external resources.
+;;; [X] I completed this assignment without assistance or external resources.
 ;;; [ ] I completed this assignment with assistance from ___
 ;;;     and/or using these external resources: ___
 
