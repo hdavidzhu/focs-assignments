7be37c7bd828ed3bdb68ae2fdc446bb52237298b
David Zhu <hdavidzhu@gmail.com>
2016-09-15 00:08:54 -0400

    ADD: Solution for part 2.

--- day4/hw3.rkt
+++ day4/hw3.rkt
@@ -43,3 +43,31 @@
 ;;
 ;; Add the `lookup-list` argument to your hw2 evaluator (or ours, from the solution set).
 ;; `(evaluate 'foo lookup-list)` should return whatever `'foo` is associated with in `lookup-list`.
+
+(define (calculate x lookup-list)
+  (match x
+    [(list operation if-expression then-expression else-expression)
+     (match operation
+       ['IPH (if (calculate if-expression lookup-list) (calculate then-expression lookup-list) (calculate else-expression lookup-list))])]
+    [(list operation left right)
+     (match operation
+       ['ADD (+ (calculate left lookup-list) (calculate right lookup-list))]
+       ['SUB (- (calculate left lookup-list) (calculate right lookup-list))]
+       ['MUL (* (calculate left lookup-list) (calculate right lookup-list))]
+       ['DIV (/ (calculate left lookup-list) (calculate right lookup-list))]
+       ['GT (> (calculate left lookup-list) (calculate right lookup-list))]
+       ['LT (< (calculate left lookup-list) (calculate right lookup-list))]
+       ['GE (>= (calculate left lookup-list) (calculate right lookup-list))]
+       ['LE (<= (calculate left lookup-list) (calculate right lookup-list))]
+       ['EQ (= (calculate left lookup-list) (calculate right lookup-list))]
+       ['NEQ (not (= (calculate left lookup-list) (calculate right lookup-list)))]
+       ['ANDD (and (calculate left lookup-list) (calculate right lookup-list))]
+       ['ORR (or (calculate left lookup-list) (calculate right lookup-list))])]
+    [(list operation operand)
+     (match operation
+       ['NOTT (not (calculate operand lookup-list))])]
+    [x (if (assq x lookup-list)
+           (second (assq x lookup-list))
+           x)]))
+
+(calculate '(IPH (GT z 0) z (SUB 0 z)) '((x 3) (y 2) (z -5))) ; -> 5
