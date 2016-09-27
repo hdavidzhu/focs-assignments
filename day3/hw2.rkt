cdb585f16fc5506ed1846c9e8724f5b85495a5d9
David Zhu <hdavidzhu@gmail.com>
2016-09-27 18:15:49 -0400

    ADD: Name signatures.

--- day3/hw2.rkt
+++ day3/hw2.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-;;; Student Name: Frankly Olin [change to your name]
+;;; Student Name: David Zhu
 ;;;
 ;;; Check one:
 ;;; [ ] I completed this assignment without assistance or external resources.
--- day4/hw3.rkt
+++ day4/hw3.rkt
@@ -1,9 +1,9 @@
 #lang racket
 
-;;; Student Name: Frankly Olin [change to your name]
+;;; Student Name: David Zhu
 ;;;
 ;;; Check one:
-;;; [ ] I completed this assignment without assistance or external resources.
+;;; [X] I completed this assignment without assistance or external resources.
 ;;; [ ] I completed this assignment with assistance from ___
 ;;;     and/or using these external resources: ___
 
